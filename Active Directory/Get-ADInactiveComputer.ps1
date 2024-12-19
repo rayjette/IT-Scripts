63b58ab4fab4641e39b0e5070f6a69504859db73
Raymond Jette <rjette@mestek.com>
2024-12-19 09:00:28 -0500

    Update doc and comments.

--- Active Directory/Get-ADInactiveComputer.ps1
+++ Active Directory/Get-ADInactiveComputer.ps1
@@ -1,54 +1,51 @@
 Function Get-ADInactiveComputer {
     <#
-        .SYNOPSIS
+    .SYNOPSIS
         Find inactive computer account objects in Active Directory.
 
-        .DESCRIPTION
-        Get-ADInactiveComputer searches for inactive Active Directory computer account objects.
-        It can filter accounts based on the number of days since the last logon,
-        accounts that have never logged on, and accounts that are disabled.
+    .DESCRIPTION
+            Get-ADInactiveComputer searches for computer accounts in Active Directory that are inactive based on the specific criteria.
 
-        .PARAMETER InactiveDays
-        Specifies the number of days since the last logon to consider a computer account inactive.
-        The default value is 90 days.
+    .PARAMETER InactiveDays
+        Specifies the number of days a computer account has been inactive.  Default is 90 days.
 
-        .PARAMETER NeverLogon
+    .PARAMETER NeverLogon
         Filters the results to include only computer accounts that have never logged on.
 
-        .PARAMETER DisabledOnly
-        Filters the result to include only computer accounts that are disabled.
+    .PARAMETER DisabledOnly
+        Finds computer accounts that are disabled.
 
-        .PARAMETER SearchBase
-        Specifies the distinguished name of the OU to search in.
+    .PARAMETER SearchBase
+        Specifies the Organizational Unit (OU) to search within.
 
-        .EXAMPLE
+    .EXAMPLE
         Get-ADInactiveComputer
         Finds Active Directory computer account objects which have been inactive for 90 days or more.
 
-        .EXAMPLE
+    .EXAMPLE
         Get-ADInactiveComputer -InactiveDays 60
         Finds Active Directory computer objects which have not been logged on for 60 days or more.
 
-        .EXAMPLE
+    .EXAMPLE
         Get-ADInactiveComputer -DisabledOnly
         Finds Active Directory computer accounts which are disabled.
 
-        .EXAMPLE
+    .EXAMPLE
         Get-ADInactiveComputer -NeverLogon
         Finds Active Directory computer accounts which have never been logged on.
 
-        .EXAMPLE
+    .EXAMPLE
         Get-ADInactiveComputer -SearchBase 'OU=Computers,DC=contoso,DC=com'
         Finds Active Directory computer accounts in the specified OU.
 
-        .INPUTS
+    .INPUTS
         None.  Get-ADInactiveComputer does not accept pipeline input.
 
-        .OUTPUTS
+    .OUTPUTS
         Microsoft.ActiveDirectory.Management.ADComputer
 
-        .NOTES
-        Raymond Jette
+    .NOTES
+        Author: Raymond Jette
     #>
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     Param (
@@ -64,6 +61,7 @@ Function Get-ADInactiveComputer {
         [string]$SearchBase
     )
 
+    # Calculate the date to filter inactive computers
     $filterDate = (Get-Date).AddDays(-$InactiveDays)
 
     # The parameter for Get-ADComputer
@@ -71,15 +69,16 @@ Function Get-ADInactiveComputer {
         Filter     = '*'
         Properties = 'LastLogonDate', 'Enabled'
     }
-    # If the SearchBase parameter was specified add it to the parameters to Get-ADComputer.
+
+    # Add SearchBase parameter if specified
     if ($PSBoundParameters.ContainsKey('SearchBase')) {
         $splat.add('SearchBase', $SearchBase)
     }
 
-    # Get computers objects from Active Directory.
+    # Retrieve computer objects from Active Directory
     $computers = Get-ADComputer @splat
     
-    # If the DisabledOnly parameter is specified we are only instrested in disabled computer objects.
+    # Filter for disabled computer objects if DisabledOnly parameter is specified
     if ($PSBoundParameters.ContainsKey('DisabledOnly')) {
         foreach ($computer in $computers) {
             if (-not ($computer.enabled)) {
@@ -87,12 +86,22 @@ Function Get-ADInactiveComputer {
             }
         }
     }
-    # If the NeverLogon parameter is present output computers which have never logged on.
+    
+    # If the NeverLogon parameter is present, output computers which have never logged on.
     elseif ($PSBoundParameters.ContainsKey('NeverLogon')) {
-        $computers | Where-Object {$null -eq $_.LastLogonDate}
+        foreach ($computer in $computers) {
+            if (-not $computer.LastLogonDate) {
+                $computer
+            }
+        }
     }
-    # Output all computers which have not logged on since or before $filterDate.
+
+    # Filter for computers that have not logged on since the filter date
     else {
-        $computers | Where-Object {$_.LastLogonDate -lt $filterDate}
+        foreach ($computer in $computers) {
+            if ($computer.LastLogonDate -lt $filterDate) {
+                $computer
+            }
+        }
     }
 } # Get-ADInactiveComputer
