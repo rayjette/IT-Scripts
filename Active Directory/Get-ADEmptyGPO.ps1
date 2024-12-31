02ee5560508df7d3b832a0d5994c7e22cbeda046
Raymond Jette <rjette@mestek.com>
2024-12-31 11:38:31 -0500

    Re-wrote Get-ADEmptyGPO

--- Active Directory/Get-ADEmptyGPO.ps1
+++ Active Directory/Get-ADEmptyGPO.ps1
@@ -13,14 +13,19 @@ Function Get-ADEmptyGPO
         If settings were modified and then revered, the GPO will not be considered empty and will no be returned.
 
     .PARAMETER OnlyLinked
-        If specified, only GPOs that are linked to an Active Directory container (such as a site, domain, or organizational unit) will be returned.
+        If specified, only GPOs that are linked to an Active Directory container will be returned.
         Unlinked GPOs will be excluded from the output.
 
     .EXAMPLE
         Get-ADEmptyGPO
 
         Retrieves all GPOs that have never been altered since their creation.
+ 
+    .EXAMPLE
+        Get-ADEmptyGPO -OnlyLinked
 
+        Retrieves all GPOs that have never been altered since their creation and are linked to an Active Directory container.
+   
     .INPUTS
         None.  Get-ADEmptyGPO does not except input via the pipeline.
 
@@ -35,29 +40,48 @@ Function Get-ADEmptyGPO
         [switch] $OnlyLinked
     )
 
-    Function Test-IsGPOLinked($gpo) {
-        <#
-        .SYNOPSIS
-            Returns true if the gpo has a link to an OU.  False otherwise.
-        #>
-        [xml] $report = Get-GPOReport -ReportType 'xml' -Name $gpo.displayname
-        $report.gpo.psobject.properties.name -contains 'LinksTo'
-    }
-    $GPOs = Get-GPO -All
+    Function Get-UnmodifiedGPOs {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = $true)]
+            [Microsoft.GroupPolicy.GPO[]] $GPOs
+        )
 
-    $filter = {$_.user.dsversion -eq 0 -and $_.computer.dsversion -eq 0}
-    $unmodifiedGpo = $GPOs | Where-Object -FilterScript $filter
-    
-    if ($PSBoundParameters.ContainsKey('OnlyLinked')) {
-        $results = @()
-        foreach ($gpo in $unmodifiedGpo) {
-            if (Test-IsGPOLinked($gpo)) {
-                $results += $gpo
+        $unmodifiedGPOs = @()
+        foreach ($gpo in $GPOs) {
+            if ($gpo.User.DSVersion -eq 0 -and $gpo.Computer.DSVersion -eq 0) {
+                $unmodifiedGPOs += $gpo
             }
         }
-    } else {
-        $results = @($unmodifiedGPO)
+        $unmodifiedGPOs
     }
-    $results
 
+
+    Function Test-IsGPOLinked {
+        param (
+            [Parameter(Mandatory = $true)]
+            [Microsoft.GroupPolicy.GPO] $GPO
+        )
+        [xml] $report = Get-GPOReport -ReportType 'xml' -Name $gpo.DisplayName
+        $report.GPO.PSObject.Properties.Name -contains 'LinksTo'
+    }
+
+    try {
+        $GPOs = Get-GPO -All
+        $unModifiedGPOs = Get-UnmodifiedGPOs -GPOs $GPOs
+
+        if ($OnlyLinked) {
+            $linkedGPOs = @()
+            foreach ($gpo in $unmodifiedGPOs) {
+                if (Test-IsGPOLinked -GPO $gpo) {
+                    $linkedGPOs += $gpo
+                }
+            }
+            $linkedGPOs
+        } else {
+            $unModifiedGPOs
+        }
+    } catch {
+        Write-Error -Message "An error occurred: $_"
+    }
 }
