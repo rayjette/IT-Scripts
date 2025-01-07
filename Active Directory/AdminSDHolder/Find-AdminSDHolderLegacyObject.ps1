047af97fb4d97ac124a91d56c53915fa3b1e0a12
Raymond Jette <rjette@mestek.com>
2025-01-07 13:36:21 -0500

    Add function to find legacy AdminSDHolder objects

--- Active Directory/AdminSDHolder/Find-AdminSDHolderLegacyObject.ps1
+++ Active Directory/AdminSDHolder/Find-AdminSDHolderLegacyObject.ps1
@@ -19,6 +19,109 @@ Function Find-AdminSDHolderLegacyObject
     [CmdletBinding()]
     param ()
 
+
+    # Retrieves all direct and indirect group memberships for a specified user or group.
+    function Get-AllGroupMembers {
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$Identity
+        )
+
+        # Helper function to get all groups a user is a member of (direct and indirect)
+        function Get-UserGroups {
+            param (
+                [Parameter(Mandatory = $true)]
+                [string]$Username
+            )
+    
+            $user = Get-ADUser -Identity $Username
+            $allGroups = @()
+    
+            # Get the user's direct group memberships
+            $groups = Get-ADUser $user -Properties MemberOf | Select-Object -ExpandProperty MemberOf
+    
+            # Loop through each group and get indirect memberships (recursively)
+            foreach ($group in $groups) {
+                $allGroups += Get-GroupMemberships -GroupDN $group
+            }
+    
+            $allGroups
+        }
+    
+        
+        # Helper to get all direct and indirect memberships of a group (recursive)
+        function Get-GroupMemberships {
+            param (
+                [Parameter(Mandatory = $true)]
+                [string]$GroupDN
+            )
+    
+            $nestedGroups = @()
+    
+            # Get the direct group itself by name (not DistinguishedName)
+            $groupName = (Get-ADGroup $GroupDN -Properties Name).Name
+            $nestedGroups += $groupName
+    
+            # Get the members of the group (to find other groups it may belong to)
+            $members = Get-ADGroupMember $GroupDN -ErrorAction SilentlyContinue
+    
+            foreach ($member in $members) {
+                if ($member.objectClass -eq "group") {
+                    # If the member is a group, add it to the list recursively by name
+                    $nestedGroups += Get-GroupMemberships -GroupDN $member.DistinguishedName
+                }
+            }
+    
+            # Now, get the groups the current group is a member of (i.e., group1 -> group2 -> group3)
+            $groupMembers = Get-ADGroup $GroupDN -Properties MemberOf | Select-Object -ExpandProperty MemberOf
+    
+            foreach ($parentGroup in $groupMembers) {
+                # Add the group names that the current group is a member of
+                $parentGroupName = (Get-ADGroup $parentGroup -Properties Name).Name
+                $nestedGroups += $parentGroupName
+            }
+    
+            $nestedGroups
+        }
+    
+        $user = $null
+        $group = $null
+        $allGroups = @()
+    
+        # Try to get user and catch errors if the user doesn't exist
+        try {
+            $user = Get-ADUser -Identity $Identity -ErrorAction Stop
+        }
+        catch {
+            # If the user is not found, do nothing, we will try as a group next
+        }
+    
+        # Try to get group and catch errors if the group doesn't exist
+        try {
+            $group = Get-ADGroup -Identity $Identity -ErrorAction Stop
+        }
+        catch {
+            # If the group is not found, do nothing
+        }
+    
+        if ($user) {
+            # If it's a user, get the user's group memberships (direct and indirect)
+            $allGroups = Get-UserGroups -Username $Identity
+        }
+        elseif ($group) {
+            # If it's a group, get the group's memberships (direct and indirect)
+            $allGroups = Get-GroupMemberships -GroupDN $group.DistinguishedName
+        }
+        else {
+            Write-Error "Identity '$Identity' is neither a valid user nor a group."
+            return
+        }
+    
+        # Return the unique list of group names (direct and indirect)
+        $allGroups | Sort-Object -Unique
+    }
+
+    
     $protectedGroups = @(
         'Account Operators',
         'Administrators',
@@ -49,7 +152,7 @@ Function Find-AdminSDHolderLegacyObject
         }
 
         # Get all the groups $object is a member of
-         $allGroupMembers = Get-ADPrincipalGroupMembership -Identity $object.SamAccountName | Select-Object -ExpandProperty Name
+        $allGroupMembers = Get-AllGroupMembers -Identity $object.SamAccountName
 
         # Check if the object object is still a member of any protected group
         $isProtected = $false
