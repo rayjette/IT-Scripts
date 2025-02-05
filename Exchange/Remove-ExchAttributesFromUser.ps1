b166bb51ee1b8afd466b9d1c1ad5b5be182d6454
Raymond Jette <rjette@mestek.com>
2025-02-05 13:11:54 -0500

    Update comments, documentation, and added verbose output.

--- Exchange/Remove-ExchAttributesFromUser.ps1
+++ Exchange/Remove-ExchAttributesFromUser.ps1
@@ -2,38 +2,33 @@ Function Remove-ExchAttributesFromUser
 {
     <#
     .SYNOPSIS
-        Can be run against an Active Directory user account to remove
-        any Exchange related attributes.
+        Removes Exchange-related attributes from an Active Directory user account.
 
     .DESCRIPTION
-        Can be run against an Active Directory user account to remove
-        any Exchange related attributes.
-
-        Removing the Exchange attributes from  an Active Directory user
-        account has the effect of deleting the mailbox.
+        This function is used to remove Exchange-related attributes from an Active Directory user account.
+        Removing these attributes from the user will effectively delete the associated Exchange mailbox.
 
     .PARAMETER samAccountName
-        The SAM name for the account.
+        The samAccountName of the user account from which Exchange-related attributes will be removed.
+        This parameter is required.
 
     .EXAMPLE
         Remove-ExchAttributesFromUser -samAccountName account_1
-        Removes the Exchange attributes from the Active Directory user account_1.
+        Removes the Exchange attributes from the Active Directory user with the samAccountName of account_1.
 
     .INPUTS
         None.  Remove-ExchAttributesFromUser does not accept input from the pipeline.
 
     .OUTPUTS
-        None.
+        None.  This function will output no results.
+
 
     .NOTES
         Author: Raymond Jette
-        Version: 1.0
+        Last Changed: 02/05/2025
         https://github.com/rayjette
     #>
-    [CmdletBinding(
-        SupportsShouldProcess, 
-        ConfirmImpact = 'High'
-    )]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
         [Parameter(
             Mandatory,
@@ -42,6 +37,8 @@ Function Remove-ExchAttributesFromUser
         [alias('id', 'user')]
         [string]$samAccountName
     )
+
+    # List of Exchange-related attributes to remove from the user account
     $attributes = @(
         'mail'
         'mailNickName'
@@ -57,11 +54,15 @@ Function Remove-ExchAttributesFromUser
         'msExchRemoteRecipientType'
         'proxyAddresses'
     )
+
+    # Check if the script should proceed with making changes
     if ($PSCmdlet.ShouldProcess($samAccountName)) {
         try {
+            # Attempt to clear Exchange-related attributes from the specified user account
             Set-AdUser -Identity $samAccountName -Clear $attributes
+            Write-Verbose "Exchange attributes removed from user account: $samAccountName"
         } catch {
-            Write-Error -Message $_.Exception.Message
+            Write-Error "Error while removing attributes for user '$samAccountName': $_.Exception.Message"
         }
     }
 } # Remove-ExchAttributesFromUser
